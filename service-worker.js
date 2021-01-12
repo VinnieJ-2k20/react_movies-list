0e99f49131e33e4d4487ba53ee0afc3bcdaef834
PaulT <ptimofejev@gmail.com>
2021-01-12 12:51:34 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.66485fb6e90efa1302da29f712c4a793.js"
 );
 
 self.addEventListener('message', (event) => {
